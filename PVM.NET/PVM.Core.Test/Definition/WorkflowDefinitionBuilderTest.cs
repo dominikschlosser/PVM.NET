45e3f9ee0a191d3e7b73833a4434b752b0e1de4e
Dominik Schlosser <dominik.schlosser@gmail.com>
2015-08-07 09:18:43 +0200

    Added AddInitialNode

--- PVM.NET/PVM.Core.Test/Definition/WorkflowDefinitionBuilderTest.cs
+++ PVM.NET/PVM.Core.Test/Definition/WorkflowDefinitionBuilderTest.cs
@@ -9,13 +9,28 @@ namespace PVM.Core.Test.Definition
     public class WorkflowDefinitionBuilderTest
     {
         [Test]
-        public void AddNode_CreatesExecutionWithSingleNode()
+        public void AddNode_CreatesDefinitionWithSingleNode()
         {
             var workflowBuilder = new WorkflowDefinitionBuilder();
+            var node = Mock.Of<INode>();
 
-            var definition = workflowBuilder.AddNode(Mock.Of<INode>()).Build();
+            var definition = workflowBuilder.AddNode(node).Build();
 
             Assert.That(definition.Nodes.Count(), Is.EqualTo(1));
+            Assert.That(definition.Nodes.Contains(node));
+        }
+
+        [Test]
+        public void AddInitialNode_CreatesDefinitionWithInitalNode()
+        {
+            var workflowBuilder = new WorkflowDefinitionBuilder();
+            var node = Mock.Of<INode>();
+
+            var definition = workflowBuilder.AddInitialNode(node).Build();
+
+            Assert.That(definition.InitialNode, Is.EqualTo(node));
+            Assert.That(definition.Nodes.Count(), Is.EqualTo(1));
+            Assert.That(definition.Nodes.Contains(node));
         }
     }
 }
--- PVM.NET/PVM.Core/Definition/WorkflowDefinitionBuilder.cs
+++ PVM.NET/PVM.Core/Definition/WorkflowDefinitionBuilder.cs
@@ -5,11 +5,19 @@ namespace PVM.Core.Definition
     public class WorkflowDefinitionBuilder
     {
         private readonly List<INode> nodes = new List<INode>();
-        private Node initialNode;
+        private INode initialNode;
 
         public WorkflowDefinitionBuilder AddNode(INode node)
         {
             nodes.Add(node);
+
+            return this;
+        }
+
+        public WorkflowDefinitionBuilder AddInitialNode(INode node)
+        {
+            nodes.Add(node);
+            initialNode = node;
 
             return this;
         }
