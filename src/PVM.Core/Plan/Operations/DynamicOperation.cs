468f2ee9bb066041f56dbe96d675dfca758db03d
Dominik Schlosser <dominik.schlosser@gmail.com>
2015-08-21 23:22:56 +0200

    Add file

---
+++ src/PVM.Core/Plan/Operations/DynamicOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PVM.Core.Data;
+using PVM.Core.Runtime;
+
+namespace PVM.Core.Plan.Operations
+{
+    public class DynamicOperation<T> : IOperation<T> where T : ICopyable<T>
+    {
+        private readonly Action<IExecution<T>> action;
+
+        public DynamicOperation(Action<IExecution<T>> action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(IExecution<T> execution)
+        {
+            action(execution);
+        }
+    }
+}
